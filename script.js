4f97f7232c7bc818254ae87a147b5da3ffc19b00
krishnamagar-web <krishna.magar1609@gmail.com>
2026-02-09 11:09:06 +0530

    Update emotional responses and button actions

--- script.js
+++ script.js
@@ -1,50 +1,69 @@
 let noCount = 0;
 
 const noTexts = [
-  "Please don't 🥺",
+  "Please don't 😢",
   "I will cry 😭",
-  "Think again 🥹",
-  "Are you sure? 😢",
-  "I really love you, think again 🫂",
-  "Gussa ho jaunga samjhi 😤"
+  "Think again 🥺",
+  "Are you sure? 😟",
+  "I really love you ❤️ Think again",
+  "Piti piti kr dunga samjhi 😤"
 ];
 
-function noClicked() {
+const question = document.getElementById("question");
+const gif = document.getElementById("gif");
+const card = document.getElementById("card");
+
+document.getElementById("noBtn").onclick = () => {
   if (noCount < noTexts.length) {
-    document.querySelector(".question").innerText = noTexts[noCount];
-    document.getElementById("gif").src =
-      "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExbGx4bHJ5d3Y4ZnlrNjA3dGQ5eXN3d3B6b2w5ZndyM3l0bWRyZCZlcD12MV9naWZzX3NlYXJjaCZjdD1n/OPU6wzx8JrHna/giphy.gif";
+    question.innerText = noTexts[noCount];
+    gif.src = "https://media.giphy.com/media/d2lcHJTG5Tscg/giphy.gif";
     noCount++;
   }
-}
+};
 
-function yesClicked() {
-  document.body.innerHTML = `
-  <div class="container">
-    <p class="question">
-    Ye hamara pehla valentine hai long distance wala but agle wale valentine me ham sath hoge.
-    Me tumhare liye gift launga badle me mujhe tumse kuch nahi chahiye kyuki mera gift to tum hi ho 🤭<br><br>
-    Tum itni khaas ho ki duniya ki koi language nahi jisse main bata pau tum mere liye kitni khaas ho 🫂🫀<br><br>
-    Tum mujhe pasand ho kyu pasand ho ye mat pucho bas pasand ho ❤️<br><br>
+document.getElementById("yesBtn").onclick = () => {
+  card.innerHTML = `
+    <h2>❤️ For You ❤️</h2>
+    <p>
+    Ye hamara pehla valentine hai long distance wala,
+    but agle wale valentine me ham sath honge.
+    Me tumhare liye gift launga,
+    badle me mujhe tumse kuch nahi chahiye
+    kyunki mera gift tum hi ho 🤭<br><br>
+
+    Tum itni khas ho ki duniya me koi aisi language nahi
+    jisse me proof kar paun 🫂🫀<br><br>
+
+    Tum mujhe pasand ho,
+    kyu pasand ho ye mat pucho,
+    bas pasand ho ❤️<br><br>
+
     Happy Valentine's Day Priyanshi 😚
     </p>
-    <button class="yes" onclick="photos()">NEXT ➡️</button>
-  </div>`;
-}
 
-function photos() {
-  document.body.innerHTML = `
-  <div class="container">
-    <h2>Our Memories 💕</h2>
+    <button class="back-btn" onclick="showPhotos()">Next ➡️</button>
+  `;
+};
 
-    <img src="IMG_20260209_095929.jpg"><p>Ye maine tumhare liye likha hua first love letter hai 🤭</p>
+function showPhotos() {
+  card.innerHTML = `
+    <h2>📸 Our Memories 💖</h2>
 
-    <img src="IMG_20260209_095944.jpg"><p>Jab ham call pr soye the 🥹🫀</p>
+    <img src="IMG_20260209_095929.jpg">
+    <p>Ye maine tumhare liye likha hua first love letter hai 🤭</p>
 
-    <img src="IMG_20260209_095914.jpg"><p>Dekho ham kitna baat karte the 🫂</p>
+    <img src="IMG_20260209_095944.jpg">
+    <p>Jab ham call pr soye the 🥹🫀</p>
 
-    <img src="Screenshot_20260209_100032.jpg"><p>Ye BGMI ki yaadein 🥰</p>
+    <img src="IMG_20260209_095914.jpg">
+    <p>Dekho ham kitna baat karte the 🫂</p>
 
-    <img src="IMG_20260209_095445.jpg"><p>Tumhare liye pehli baar phool banaya tha 🤭</p>
-  </div>`;
+    <img src="Screenshot_20260209_100032.jpg">
+    <p>Ye BGMI ki yaadein 🥰</p>
+
+    <img src="IMG_20260209_095445.jpg">
+    <p>Tumhare liye pehli baar phool banaya tha 🤭</p>
+
+    <button class="back-btn" onclick="location.reload()">⬅️ Back</button>
+  `;
 }
